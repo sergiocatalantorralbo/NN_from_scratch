a4ac3ccd4c7424019d551f774a2a53134585641a
Sergio Catalán Torralbo~ <sergio_catalan_torralbo@carrefour.com>
2024-01-23 14:43:37 +0100

    First code structure

---
+++ utils/activation_functions.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+    s = 1/(1+np.exp(-z))
+
+    return s
